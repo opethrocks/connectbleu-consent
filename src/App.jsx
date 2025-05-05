f6a807cb1b534a15b37e89d8a102ed0bdec84006
Matt <gatorgoat@protonmail.com>
2025-05-05 12:30:27 -0500

    use telnyx for # verification, style accordingly

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,67 @@
 import { useState } from "react";
+import axios from "axios";
 import connectBleuLogo from "./assets/internet.png";
 import phoneIcon from "./assets/phone.svg";
 import mailIcon from "./assets/mail.svg";
 import checkIcon from "./assets/check.svg";
 import subscribeIcon from "./assets/subscribe.svg";
-
 import "./App.css";
 
 function App() {
   const [userInput, setUserInput] = useState("Phone Number");
-  const [isSubscribed, setIsSubscribed] = useState(false);
+  const [isSubscribed, setIsSubscribed] = useState();
+  const [isValidNumber, setIsValidNumber] = useState();
+  const [invalidInput, setInvalidInput] = useState();
 
-  const handleFocus = () => {
-    setUserInput("");
-    setIsSubscribed(false);
+  const phoneNumberVerification = async () => {
+    try {
+      const {
+        data: {
+          data: { valid_number: validNumber },
+        },
+      } = await axios.get(
+        `https://api.telnyx.com/v2/number_lookup/${userInput}`,
+        {
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_TELNYX_API_KEY}`,
+            Accept: "application/json",
+          },
+          params: {
+            phone_number: userInput,
+          },
+        }
+      );
+      setIsValidNumber(validNumber);
+    } catch ({ response: { data } }) {
+      setInvalidInput(data.errors[0].detail);
+    }
   };
 
-  const handleBlur = () => {
-    setUserInput("Phone Number");
-    setIsSubscribed(false);
+  const handleFocus = () => {
+    setUserInput("+1");
+    setInvalidInput();
+    setIsValidNumber();
   };
 
   const handleChange = (event) => {
     setUserInput(event.target.value);
   };
 
-  const handleClick = () => {
-    setIsSubscribed((prevState) => !prevState);
+  const handleClick = async () => {
+    await phoneNumberVerification();
   };
 
   const SuccessMessage = () => {
     return (
-      <div>
-        <p>You are now subscribed to Assistext!</p>
-      </div>
+      <>
+        <p>
+          {isValidNumber
+            ? "You are now subscribed to Assistext!"
+            : invalidInput
+            ? invalidInput
+            : "Please enter 10 digit phone number without dashes"}
+        </p>
+      </>
     );
   };
 
@@ -47,20 +75,14 @@ function App() {
       <h1>ConnectBleu</h1>
 
       <div className="card">
-        <div>
-          {isSubscribed ? (
-            <SuccessMessage />
-          ) : (
-            <p>Enter your phone number to subscribe</p>
-          )}
-        </div>
+        <SuccessMessage />
 
         <div>
           <span className="phone-icon-container">
-            {isSubscribed ? (
+            {isValidNumber ? (
               <img className="subscribed-icon" src={checkIcon} />
             ) : (
-              <img class="phone-icon" src={phoneIcon} />
+              <img className="phone-icon" src={phoneIcon} />
             )}
           </span>
           <input
@@ -68,9 +90,14 @@ function App() {
             id="phone"
             onInput={handleChange}
             onFocus={handleFocus}
-            onBlur={handleBlur}
             value={userInput}
-            className={isSubscribed ? "input-success" : ""}
+            className={
+              invalidInput
+                ? "input-invalid"
+                : isValidNumber
+                ? "input-success"
+                : ""
+            }
           />
         </div>
         <span className="subscribe-icon-container">
@@ -78,7 +105,13 @@ function App() {
         </span>
         <button
           onClick={handleClick}
-          className={isSubscribed ? "button-success" : ""}
+          className={
+            invalidInput
+              ? "button-invalid"
+              : isValidNumber
+              ? "button-success"
+              : ""
+          }
         >
           Subscribe
         </button>
@@ -90,14 +123,13 @@ function App() {
           out. Reply HELP for help. We will not share mobile information with
           third parties for promotional or marketing purposes.
         </p>
-        <p>
-          <div className="contact-info">
-            <a href="mailto:matt@connectbleu.com" className="email">
-              <img className="contact-icon" src={mailIcon} />
-              matt@connectbleu.com
-            </a>
-          </div>
-        </p>
+
+        <div className="contact-info">
+          <a href="mailto:matt@connectbleu.com" className="email">
+            <img className="contact-icon" src={mailIcon} />
+            matt@connectbleu.com
+          </a>
+        </div>
       </div>
     </>
   );
